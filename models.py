64ff3e31612e5d58a6cd8b2163784d2aec886980
Sandy <sangzicao@gmail.com>
2019-02-25 11:59:38 -0800

    initial commit

---
+++ models.py
@@ -0,0 +1,26 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+class Cupcake(db.Model):
+    """This class makes cupcake instances"""
+
+    __tablename__ = "cupcakes"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    flavor = db.Column(db.Text,
+                       nullable=False)
+    size = db.Column(db.Text,
+                     nullable=False)
+    rating = db.Column(db.Float,
+                       nullable=False)
+    image = db.Column(db.Text,
+                      nullable=False,
+                      default="https://tinyurl.com/truffle-cupcake")
+
